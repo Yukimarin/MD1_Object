0a104e5119e575e0034bf00b470167bfb70c13ea
Nguyễn Xuân Bách <110024359+Yukimarin@users.noreply.github.com>
2023-07-04 07:59:37 +0700

    Add files via upload

--- S13_Chuabaitap/app.js
+++ S13_Chuabaitap/app.js
@@ -46,65 +46,227 @@
 // console.log("Sau khi xóa nhân viên id =1", company);
 
 // Bài 4 (BTTH)
-let courses = [
-  { id: 1, courseName: "HTML", complete: false },
-  { id: 2, courseName: "CSS", complete: false },
-  { id: 3, courseName: "Basic of Javascript", complete: false },
-  { id: 4, courseName: "NPM", complete: false },
-  { id: 5, courseName: "Git", complete: false },
-];
-// In dữ liệu
-function render() {
-  for (let i = 0; i < courses.length; i++) {
-    console.log(`${i + 1}.${courses[i].courseName} `);
-    console.log(`Complete:${courses[i].complete}`);
+// let courses = [
+//   { id: 1, courseName: "HTML", complete: false },
+//   { id: 2, courseName: "CSS", complete: false },
+//   { id: 3, courseName: "Basic of Javascript", complete: false },
+//   { id: 4, courseName: "NPM", complete: false },
+//   { id: 5, courseName: "Git", complete: false },
+// ];
+// // In dữ liệu
+// function render() {
+//   for (let i = 0; i < courses.length; i++) {
+//     console.log(`${i + 1}.${courses[i].courseName} `);
+//     console.log(`Complete:${courses[i].complete}`);
+//   }
+// }
+// let state = true;
+// while (state) {
+//   // Cho người dùng nhập vào CRUDE
+//   let input = prompt("Nhập vào C/R/U/D/E").toUpperCase();
+//   if (input == "C") {
+//     // Cho người dùng nhập vào tên khóa học mới
+//     let addNameCourse = prompt("Nhập tên khóa học muốn thêm").toUpperCase();
+//     // Khai báo 1 đối tượng khóa học chứa các thuộc tính id, courseName,complete
+//     let addCouse = {
+//       id: Math.floor(Math.random() * 10000),
+//       courseName: addNameCourse,
+//       complete: false,
+//     };
+//     //   Thêm cái đối tượng vừa tạo vào mảng courses (các khóa học)
+//     courses.push(addCouse);
+//     console.log("Sau khi thêm khóa học");
+//     render();
+//   } else if (input == "R") {
+//     console.log("In lại danh sách khóa học");
+//     render();
+//   } else if (input == "U") {
+//     let inputUpdate = Number(prompt("Nhập vào vị trí muốn update"));
+//     if (inputUpdate - 1 < courses.length && 0 <= inputUpdate - 1) {
+//       let updateNameCourse = prompt(
+//         "Nhập vào tên khóa học muốn update"
+//       ).toUpperCase();
+//       // inputUpdate-1 chính là vị trí index của khóa học muốn sửa trong mảng khóa học
+//       courses[inputUpdate - 1].courseName = updateNameCourse;
+//       // Update luôn complete thành true
+//       courses[inputUpdate - 1].complete = true;
+//     } else {
+//       console.log("Không tìm thấy");
+//     }
+//     console.log("Sau khi update");
+//     render();
+//   } else if (input == "D") {
+//     let inputDelete = Number(prompt("Nhập vào vị trí muốn xóa"));
+//     // Tam bỏ qua điều kiện tồn tại
+//     courses.splice(inputDelete - 1, 1);
+//     render();
+//   } else if (input == "E") {
+//     console.log("Cảm ơn bạn đã sử dụng app");
+//     state = false;
+//     render();
+//   } else {
+//     alert("Nhập lại đi bạn ");
+//   }
+// }
+
+// Bài 2 (bài tập)
+// let student = {
+//   id: 1,
+//   name: "Nguyễn Văn A",
+//   age: 20,
+//   gender: "Nam",
+//   mark: 1,
+// };
+
+// let newStudent = {
+//   id: 2,
+//   name: "Nguyễn Văn B",
+//   age: 20,
+//   gender: "Nam",
+//   mark: 9,
+// };
+
+// let students = [];
+// students.push(student, newStudent);
+// console.log(students);
+
+// let max = students[0].mark;
+// for (let i = 0; i < students.length; i++) {
+//   if (students[i].mark >= max) {
+//     max = students[i].mark;
+//   }
+// }
+
+// let findIndex = students.findIndex((element) => element.mark == max);
+// console.log(students[findIndex]);
+
+// let newStudent2 = {
+//   id: 3,
+//   name: "Nguyễn Văn C",
+//   age: 20,
+//   gender: "Nam",
+//   mark: 5,
+// };
+// students.push(newStudent2);
+// console.log("Sau khi thêm sinh viên thứ 3", students);
+// // Buoc 1: tính tổng điểm các học sinh
+// let sum = 0;
+// for (let i = 0; i < students.length; i++) {
+//   sum = sum + students[i].mark;
+// }
+// let average = sum / students.length;
+// console.log(average);
+
+// if(average>7.5){
+
+// }
+
+// Bài 1 (BTTH)
+// let products = [
+//   { id: 1, name: "Butter", count: 100 },
+//   { id: 2, name: "Yakult", count: 100 },
+//   { id: 3, name: "Butter", count: 100 },
+// ];
+
+// let key = prompt("Nhap vao tu khoa can tim");
+// let check = -1;
+// for (let i = 0; i < products.length; i++) {
+//   if (products[i].name == key) {
+//     check = i;
+//   }
+// }
+
+// if (check == -1) {
+//   console.log("Không tìm thấy");
+// } else {
+//   console.log(products[check]);
+// }
+
+// Cách 2: find hoặc là findIndex
+// let findIndex = products.findIndex((element) => element.name == key);
+// console.log(findIndex);
+// let find = products.find((element) => element.name == key);
+// console.log(find);
+
+// Bài 3:
+// Bước 1: Tạo ra bản nguyên mẫu User (id,name, email, address,phone)
+class User {
+  constructor(id, name, email, address, phone) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.address = address;
+    this.phone = phone;
+    this.role = 0;
   }
 }
-let state = true;
-while (state) {
-  // Cho người dùng nhập vào CRUDE
-  let input = prompt("Nhập vào C/R/U/D/E").toUpperCase();
-  if (input == "C") {
-    // Cho người dùng nhập vào tên khóa học mới
-    let addNameCourse = prompt("Nhập tên khóa học muốn thêm").toUpperCase();
-    // Khai báo 1 đối tượng khóa học chứa các thuộc tính id, courseName,complete
-    let addCouse = {
-      id: Math.floor(Math.random() * 10000),
-      courseName: addNameCourse,
-      complete: false,
-    };
-    //   Thêm cái đối tượng vừa tạo vào mảng courses (các khóa học)
-    courses.push(addCouse);
-    console.log("Sau khi thêm khóa học");
-    render();
-  } else if (input == "R") {
-    console.log("In lại danh sách khóa học");
-    render();
-  } else if (input == "U") {
-    let inputUpdate = Number(prompt("Nhập vào vị trí muốn update"));
-    if (inputUpdate - 1 < courses.length && 0 <= inputUpdate - 1) {
-      let updateNameCourse = prompt(
-        "Nhập vào tên khóa học muốn update"
-      ).toUpperCase();
-      // inputUpdate-1 chính là vị trí index của khóa học muốn sửa trong mảng khóa học
-      courses[inputUpdate - 1].courseName = updateNameCourse;
-      // Update luôn complete thành true
-      courses[inputUpdate - 1].complete = true;
-    } else {
-      console.log("Không tìm thấy");
-    }
-    console.log("Sau khi update");
-    render();
-  } else if (input == "D") {
-    let inputDelete = Number(prompt("Nhập vào vị trí muốn xóa"));
-    // Tam bỏ qua điều kiện tồn tại
-    courses.splice(inputDelete - 1, 1);
-    render();
-  } else if (input == "E") {
-    console.log("Cảm ơn bạn đã sử dụng app");
-    state = false;
-    render();
-  } else {
-    alert("Nhập lại đi bạn ");
+// Bước 2: Tạo ra bản nguyên mẫu Admin kế thừa từ User, thêm role =1
+class Admin extends User {
+  constructor(id, name, email, address, phone) {
+    super(id, name, email, address, phone);
+    this.role = 1;
+  }
+}
+// Bước 3: Tạo ra mảng users rỗng và thêm 3 user + 1 admin
+let users = [];
+
+users.push(new User(1, "Nguyễn Văn A", "A@gmail.com", "Hà Nội", "0123456"));
+users.push(new User(2, "Nguyễn Văn B", "B@gmail.com", "Hà Nội", "0123456"));
+users.push(new User(3, "Nguyễn Văn C", "C@gmail.com", "Hà Nội", "0123456"));
+users.push(new Admin(4, "Nguyễn Văn D", "D@gmail.com", "Hà Nội", "0123456"));
+
+console.log("Sau khi thêm user", users);
+// Bước 4: Xóa người dùng thông qua tên (không xóa thằng admin)
+// let input = prompt("Nhập vào tên muốn xóa");
+// let check = -1;
+// for (let i = 0; i < users.length; i++) {
+//   if (users[i].name == input && users[i].role != 1) {
+//     check = i;
+//   }
+// }
+// // Ra ngoài vòng for, sử dụng mệnh đề điều kiện với biến check
+// if (check == -1) {
+//   console.log("Không tìm thấy tên người dùng");
+// } else {
+//   users.splice(check, 1);
+// }
+
+// console.log("Sau khi xóa tên người dùng", users);
+// Bước 5: Sửa thông tin thông qua id nhưng không sửa admin
+// let inputID = Number(prompt("Nhập vào ID muốn sửa"));
+// let checkID = -1;
+// for (let i = 0; i < users.length; i++) {
+//   if (users[i].id == inputID && users[i].role != 1) {
+//     checkID = i;
+//   }
+// }
+// if (checkID == -1) {
+//   console.log("Không tìm thấy kết quả");
+// } else {
+//   let inputUpdate = prompt("Nhập vào tên muốn sửa");
+//   users[checkID].name = inputUpdate;
+// }
+
+// console.log(users);
+// Bước 6:
+// Thêm 1 nick admin vào mảng danh sách người dùng.
+// Sau đó tìm kiếm toàn bộ thông tin các tài khoản là admin và in ra
+users.push(new Admin(5, "Nguyễn Văn E", "E@gmail.com", "Hà Nội", "0123456"));
+console.log("Sau khi thêm admin", users);
+// Tìm và in ra toàn bộ tài khoản admin
+for (let i = 0; i < users.length; i++) {
+  if (input == users[i].name) {
+  }
+  if (users[i].role == 1) {
+    console.log("Thông tin tài khoản admin", users[i]);
+  }
+}
+
+let check = true;
+while (check) {
+  let input = prompt("Nhập ký tự của bạn: ");
+  if (input == "E") {
+    check == false;
+    console.log("Cảm ơn .... ");
   }
 }
--- Session13/app.js
+++ Session13/app.js
@@ -1,29 +1,65 @@
-// 1. Khái niệm về Object (đối tượng)
-// Các thuộc tính của 1 sinh viên: mã sinh viên, tên, tuổi, địa chỉ, khóa học, ngành, có người yêu
-//  Là kiểu dữ liễu lưu trữ các thuộc của một đối tượng
-// 2. Cách khai báo object
-let student = {
-  id: 1,
-  name: "Bách",
-  age: 32,
-  address: "Hà Nội",
-};
-console.log("Log đối tượng student", student);
-// 3. Thao tác CRUD với kiểu dữ liệu Object
-// R (Read one - Read All)
-// Read one
-// Cú pháp: object_name.key => value tương ứng với key
-console.log("Lấy giá trị của thuộc tính name", student.name);
-// Read All
-for (const key in student) {
-  console.log(`${key}:${student[key]}`);
-}
-// C (Create)- Thêm mới thuộc tính cho đối tượng
-student.email = "bachnx@rikkeisoft.com";
-console.log("Sau khi thêm thuộc tính email", student);
-// U (Update)- Thay đổi value cho thuộc tính (key) của đổi tượng
-student.age = 18;
-console.log("Sau khi update age bằng 18", student);
-// D (Delete) - Xóa 1 thuộc tính của đối tượng
-delete student.address;
-console.log("sau khi xóa thuộc tính address của đối tượng", student);
+// // 1. Khái niệm về Object (đối tượng)
+// // Các thuộc tính của 1 sinh viên: mã sinh viên, tên, tuổi, địa chỉ, khóa học, ngành, có người yêu
+// //  Là kiểu dữ liễu lưu trữ các thuộc của một đối tượng
+// // 2. Cách khai báo object
+// let student = {
+//   id: 1,
+//   name: "Bách",
+//   age: 32,
+//   address: "Hà Nội",
+// };
+// console.log("Log đối tượng student", student);
+// // 3. Thao tác CRUD với kiểu dữ liệu Object
+// // R (Read one - Read All)
+// // Read one
+// // Cú pháp: object_name.key => value tương ứng với key
+// console.log("Lấy giá trị của thuộc tính name", student.name);
+// // Read All
+// for (const key in student) {
+//   console.log(`${key}:${student[key]}`);
+// }
+// // C (Create)- Thêm mới thuộc tính cho đối tượng
+// student.email = "bachnx@rikkeisoft.com";
+// console.log("Sau khi thêm thuộc tính email", student);
+// // U (Update)- Thay đổi value cho thuộc tính (key) của đổi tượng
+// student.age = 18;
+// console.log("Sau khi update age bằng 18", student);
+// // D (Delete) - Xóa 1 thuộc tính của đối tượng
+// delete student.address;
+// console.log("sau khi xóa thuộc tính address của đối tượng", student);
+
+// Constructor Function (tên viết hoa không bắt buộc nhưng quy ước chung thì sẽ viết Hoa)
+// function User(lastName, firstName, age) {
+//   this.lastName = lastName;
+//   this.firstName = firstName;
+//   this.age = age;
+// }
+
+// let student = new User("Nguyễn", "Bách", 20);
+// let student2 = new User("Ninh", "Hoàng", 7);
+// console.log(student);
+// console.log(student2);
+
+// Object constructor (tạm dịch - xây dựng đối tượng)
+// Từ khóa class - extends(kế thừa)
+// class User {
+//   constructor(lastName, firstName, age) {
+//     this.lastName = lastName;
+//     this.firstName = firstName;
+//     this.age = age;
+//   }
+// }
+
+// let user = new User("Nguyễn", "Bách", 32);
+// console.log(user);
+
+// class Admin extends User {
+//   constructor(lastName, firstName, age) {
+//     // Phương thức super()
+//     super(lastName, firstName, age);
+//   }
+// }
+
+// let admin = new Admin("Ninh", "Hoàng", 10);
+// let admin2 = new Admin("Lê", "Thủy", 18);
+// console.log(admin2);
